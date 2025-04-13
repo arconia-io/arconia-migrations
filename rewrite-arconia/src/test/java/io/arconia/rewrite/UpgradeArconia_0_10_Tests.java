420bd1925dda4ff5819980c2c7a19779b01438b1
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-04-13 16:16:15 +0200

    test: Fix tests for YAML property change

--- rewrite-arconia/src/test/java/io/arconia/rewrite/UpgradeArconia_0_10_Tests.java
+++ rewrite-arconia/src/test/java/io/arconia/rewrite/UpgradeArconia_0_10_Tests.java
@@ -6,7 +6,7 @@ import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.yaml.Assertions.yaml;
 
-class UpgradeArconia_0_10 implements RewriteTest {
+class UpgradeArconia_0_10_Tests implements RewriteTest {
 
     @Override
     public void defaults(RecipeSpec spec) {
@@ -43,37 +43,8 @@ class UpgradeArconia_0_10 implements RewriteTest {
                         """,
                         """
                         arconia:
-                          config:
-                            profiles:
-                              development: dev
-                              test: local
-                        """,
-                        s -> s.path("src/main/resources/application.yml"))
-        );
-    }
-
-    @Test
-    void yamlKeysAreOldVariant() {
-        rewriteRun(
-                //language=yaml
-                yaml(
-                        """
-                        arconia:
-                          dev:
-                            profiles:
-                              development: dev
-                              test: local
-                          config:
-                            profiles:
-                              production: prod
-                        """,
-                        """
-                        arconia:
-                          config:
-                            profiles:
-                              development: dev
-                              test: local
-                              production: prod
+                          config.profiles.development: dev
+                          config.profiles.test: local
                         """,
                         s -> s.path("src/main/resources/application.yml"))
         );
