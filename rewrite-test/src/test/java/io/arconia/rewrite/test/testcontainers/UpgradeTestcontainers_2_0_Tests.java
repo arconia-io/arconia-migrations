40299458efc91097a50e0bebb52ea6679872fb74
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-10-31 17:05:32 +0100

    feat: Add Testcontainers 2.0 upgrade recipe
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

---
+++ rewrite-test/src/test/java/io/arconia/rewrite/test/testcontainers/UpgradeTestcontainers_2_0_Tests.java
@@ -0,0 +1,50 @@
+package io.arconia.rewrite.test.testcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+/**
+ * Unit tests for "io.arconia.rewrite.test.testcontainers.UpgradeTestcontainers_2_0".
+ */
+class UpgradeTestcontainers_2_0_Tests implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipeFromResources("io.arconia.rewrite.test.testcontainers.UpgradeTestcontainers_2_0")
+                .parser(JavaParser.fromJavaVersion().classpathFromResources(new InMemoryExecutionContext(),
+                        "testcontainers-1.21.*"));
+    }
+
+    @Test
+    void methodNameChanges() {
+        rewriteRun(
+                //language=java
+                java(
+                        """
+                        import org.testcontainers.containers.ContainerState;
+
+                        class Demo {
+                            String example(ContainerState containerState) {
+                                return containerState.getContainerIpAddress();
+                            }
+                        }
+                        """,
+                        """
+                        import org.testcontainers.containers.ContainerState;
+
+                        class Demo {
+                            String example(ContainerState containerState) {
+                                return containerState.getHost();
+                            }
+                        }
+                        """
+                )
+        );
+    }
+
+}
