25ea03b1e2ea04584021676b7a3b8465d04ee487
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-11-26 21:23:07 +0100

    feat: Recipe for upgrading to Docling Java 0.2
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

---
+++ rewrite-arconia/src/test/java/io/arconia/rewrite/docling/UpgradeDocling_0_2_Tests.java
@@ -0,0 +1,52 @@
+package io.arconia.rewrite.docling;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class UpgradeDocling_0_2_Tests implements RewriteTest {
+
+    @Test
+    void recipeConfigured() {
+        assertRecipesConfigure("io.arconia.rewrite.docling.UpgradeDocling_0_2");
+    }
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipeFromResources("io.arconia.rewrite.docling.UpgradeDocling_0_2")
+                .parser(JavaParser.fromJavaVersion().classpathFromResources(new InMemoryExecutionContext(),
+                        "docling-serve-api-0.1.*", "docling-serve-client-0.1.*"));
+    }
+
+    @Test
+    void packageChanges() {
+        rewriteRun(
+                //language=java
+                java(
+                        """
+                        import ai.docling.api.serve.DoclingServeApi;
+                        import ai.docling.client.serve.DoclingServeClient;
+
+                        class Demo {
+                            DoclingServeApi doclingServeApi = null;
+                            DoclingServeClient doclingServeClient = null;
+                        }
+                        """,
+                        """
+                        import ai.docling.serve.api.DoclingServeApi;
+                        import ai.docling.serve.client.DoclingServeClient;
+
+                        class Demo {
+                            DoclingServeApi doclingServeApi = null;
+                            DoclingServeClient doclingServeClient = null;
+                        }
+                        """
+                )
+        );
+    }
+
+}
