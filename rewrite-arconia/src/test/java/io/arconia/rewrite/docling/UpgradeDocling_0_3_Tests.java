b1e90b1bebb9711e4e3ccf6c4aaa23fe319a91e3
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-12-08 23:19:06 +0100

    feat: Add Docling Java 0.3.0 recipe
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

---
+++ rewrite-arconia/src/test/java/io/arconia/rewrite/docling/UpgradeDocling_0_3_Tests.java
@@ -0,0 +1,48 @@
+package io.arconia.rewrite.docling;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class UpgradeDocling_0_3_Tests implements RewriteTest {
+
+    @Test
+    void recipeConfigured() {
+        assertRecipesConfigure("io.arconia.rewrite.docling.UpgradeDocling_0_3");
+    }
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipeFromResources("io.arconia.rewrite.docling.UpgradeDocling_0_3")
+                .parser(JavaParser.fromJavaVersion().classpathFromResources(new InMemoryExecutionContext(),
+                        "docling-core-0.2.*"));
+    }
+
+    @Test
+    void packageChanges() {
+        rewriteRun(
+                //language=java
+                java(
+                        """
+                        import ai.docling.api.core.DoclingDocument;
+
+                        class Demo {
+                            DoclingDocument document = null;
+                        }
+                        """,
+                        """
+                        import ai.docling.core.DoclingDocument;
+
+                        class Demo {
+                            DoclingDocument document = null;
+                        }
+                        """
+                )
+        );
+    }
+
+}
