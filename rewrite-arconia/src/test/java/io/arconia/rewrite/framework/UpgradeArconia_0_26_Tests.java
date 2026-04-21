967c1816e236464cc407c63105e656068801409f
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2026-04-22 00:24:38 +0200

    feat: Recipe to upgrade to Arconia 0.26
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

---
+++ rewrite-arconia/src/test/java/io/arconia/rewrite/framework/UpgradeArconia_0_26_Tests.java
@@ -0,0 +1,141 @@
+package io.arconia.rewrite.framework;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.InMemoryExecutionContext;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+import static org.openrewrite.properties.Assertions.properties;
+
+class UpgradeArconia_0_26_Tests implements RewriteTest {
+
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipeFromResources("io.arconia.rewrite.UpgradeArconia_0_26")
+            .parser(JavaParser.fromJavaVersion().classpathFromResources(new InMemoryExecutionContext(),
+                "arconia-multitenancy-core-0.25"));
+    }
+
+    @Test
+    void typeChanges() {
+        rewriteRun(
+                //language=java
+                java(
+                        """
+                        import io.arconia.multitenancy.core.exceptions.TenantResolutionException;
+                        import io.arconia.multitenancy.core.context.events.MdcTenantContextEventListener;
+                        import io.arconia.multitenancy.core.events.TenantEvent;
+                        import io.arconia.multitenancy.core.autoconfigure.tenantdetails.TenantDetailsConfiguration;
+                        import io.arconia.multitenancy.core.autoconfigure.tenantdetails.TenantDetailsProperties;
+                        import io.arconia.multitenancy.core.autoconfigure.tenantdetails.PropertiesTenantDetailsService;
+                        import io.arconia.multitenancy.core.events.TenantEventPublisher;
+                        import io.arconia.multitenancy.core.events.DefaultTenantEventPublisher;
+                        import io.arconia.multitenancy.core.context.TenantContextHolder;
+
+                        class Demo {
+                            TenantResolutionException ex;
+                            MdcTenantContextEventListener listener;
+                            TenantEvent event;
+                            TenantDetailsConfiguration config;
+                            TenantDetailsProperties props;
+                            PropertiesTenantDetailsService service;
+                            TenantEventPublisher publisher;
+                            DefaultTenantEventPublisher defaultPublisher;
+                            TenantContextHolder holder;
+                        }
+                        """,
+                        """
+                        import io.arconia.multitenancy.core.autoconfigure.PropertiesTenantDetailsService;
+                        import io.arconia.multitenancy.core.autoconfigure.TenantDetailsConfiguration;
+                        import io.arconia.multitenancy.core.autoconfigure.TenantDetailsProperties;
+                        import io.arconia.multitenancy.core.context.TenantContext;
+                        import io.arconia.multitenancy.core.exceptions.TenantVerificationException;
+                        import io.arconia.multitenancy.core.observability.MdcTenantEventListener;
+                        import org.springframework.context.ApplicationEventPublisher;
+                        import io.arconia.multitenancy.core.context.events.TenantEvent;
+
+                        class Demo {
+                            TenantVerificationException ex;
+                            MdcTenantEventListener listener;
+                            TenantEvent event;
+                            TenantDetailsConfiguration config;
+                            TenantDetailsProperties props;
+                            PropertiesTenantDetailsService service;
+                            ApplicationEventPublisher publisher;
+                            ApplicationEventPublisher defaultPublisher;
+                            TenantContext holder;
+                        }
+                        """
+                )
+        );
+    }
+
+    @Test
+    void methodChanges() {
+        rewriteRun(
+                //language=java
+                java(
+                        """
+                        import io.arconia.multitenancy.core.tenantdetails.TenantDetails;
+                        import io.arconia.multitenancy.core.tenantdetails.Tenant;
+
+                        class Demo {
+                            void call(TenantDetails details) {
+                                details.getIdentifier();
+                                details.isEnabled();
+                                details.getAttributes();
+                            }
+                            void anotherCall(Tenant tenant) {
+                                tenant.getIdentifier();
+                                tenant.isEnabled();
+                                tenant.getAttributes();
+                            }
+                        }
+                        """,
+                        """
+                        import io.arconia.multitenancy.core.tenantdetails.TenantDetails;
+                        import io.arconia.multitenancy.core.tenantdetails.Tenant;
+
+                        class Demo {
+                            void call(TenantDetails details) {
+                                details.identifier();
+                                details.enabled();
+                                details.attributes();
+                            }
+                            void anotherCall(Tenant tenant) {
+                                tenant.identifier();
+                                tenant.enabled();
+                                tenant.attributes();
+                            }
+                        }
+                        """
+                )
+        );
+    }
+
+    @Test
+    void propertyChanges() {
+        rewriteRun(
+                //language=properties
+                properties(
+                        """
+                        arconia.multitenancy.management.mdc.enabled=true
+                        arconia.multitenancy.management.mdc.key=tenant-id
+                        arconia.multitenancy.management.observations.enabled=true
+                        arconia.multitenancy.management.observations.key=tenant-id
+                        arconia.multitenancy.management.observations.cardinality=low
+                        """,
+                        """
+                        arconia.multitenancy.logging.mdc.enabled=true
+                        arconia.multitenancy.logging.mdc.key-name=tenant-id
+                        arconia.multitenancy.observations.enabled=true
+                        arconia.multitenancy.observations.key-name=tenant-id
+                        arconia.multitenancy.observations.cardinality=low
+                        """,
+                        s -> s.path("src/main/resources/application.properties"))
+        );
+    }
+
+}
