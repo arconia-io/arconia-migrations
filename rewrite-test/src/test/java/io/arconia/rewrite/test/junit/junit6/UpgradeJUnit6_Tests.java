54dae393ea6b76674c2cdb2b475e9be676741d19
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2026-06-11 16:38:13 +0200

    feat: Enhance JUnit 6 recipes
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- rewrite-test/src/test/java/io/arconia/rewrite/test/junit/junit6/UpgradeJUnit6_Tests.java
+++ rewrite-test/src/test/java/io/arconia/rewrite/test/junit/junit6/UpgradeJUnit6_Tests.java
@@ -1,16 +1,60 @@
 package io.arconia.rewrite.test.junit.junit6;
 
 import org.junit.jupiter.api.Test;
+import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.gradle.Assertions.buildGradle;
+import static org.openrewrite.gradle.toolingapi.Assertions.withToolingApi;
 
 /**
  * Unit tests for "io.arconia.rewrite.test.junit.UpgradeJUnit_6".
  */
 class UpgradeJUnit6_Tests implements RewriteTest {
 
+    @Override
+    public void defaults(RecipeSpec spec) {
+        spec.recipeFromResources("io.arconia.rewrite.test.junit.UpgradeJUnit_6");
+    }
+
     @Test
-    void recipeConfigured() {
-        assertRecipesConfigure("io.arconia.rewrite.test.junit.UpgradeJUnit_6");
+    void dependencyChanges() {
+        rewriteRun(
+                spec -> spec.beforeRecipe(withToolingApi()),
+                //language=groovy
+                buildGradle(
+                        """
+                        plugins {
+                            id "java-library"
+                        }
+
+                        repositories {
+                            mavenCentral()
+                        }
+
+                        dependencies {
+
+                            testImplementation "org.junit.platform:junit-platform-jfr"
+                            testImplementation "org.junit.platform:junit-platform-runner"
+                            testImplementation "org.junit.platform:junit-platform-suite-commons"
+                        }
+                        """,
+                        """
+                        plugins {
+                            id "java-library"
+                        }
+
+                        repositories {
+                            mavenCentral()
+                        }
+
+                        dependencies {
+
+                            testRuntimeOnly "org.junit.platform:junit-platform-launcher"
+                        }
+                        """
+                )
+        );
     }
 
 }
