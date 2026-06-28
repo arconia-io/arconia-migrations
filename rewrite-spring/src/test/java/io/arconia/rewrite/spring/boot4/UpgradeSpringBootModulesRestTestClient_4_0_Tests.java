2abfe94983a32771cc1f91bb68731df6d568f8dd
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2026-06-28 14:50:13 +0200

    fix: Add missing dependency when TestRestTemplate used in SB4
    Fixes gh-170
    
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- rewrite-spring/src/test/java/io/arconia/rewrite/spring/boot4/UpgradeSpringBootModulesRestTestClient_4_0_Tests.java
+++ rewrite-spring/src/test/java/io/arconia/rewrite/spring/boot4/UpgradeSpringBootModulesRestTestClient_4_0_Tests.java
@@ -7,7 +7,11 @@ import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
+import static org.openrewrite.gradle.Assertions.buildGradle;
+import static org.openrewrite.gradle.toolingapi.Assertions.withToolingApi;
 import static org.openrewrite.java.Assertions.java;
+import static org.openrewrite.java.Assertions.mavenProject;
+import static org.openrewrite.java.Assertions.srcTestJava;
 
 class UpgradeSpringBootModulesRestTestClient_4_0_Tests implements RewriteTest {
 
@@ -15,7 +19,7 @@ class UpgradeSpringBootModulesRestTestClient_4_0_Tests implements RewriteTest {
     public void defaults(RecipeSpec spec) {
         spec.recipeFromResources("io.arconia.rewrite.spring.boot4.MigrateRestTestClientModule")
                 .parser(JavaParser.fromJavaVersion().classpathFromResources(new InMemoryExecutionContext(),
-                        "spring-boot-test-3.5"));
+                        "spring-boot-test-3.5", "spring-boot-resttestclient-4.0"));
     }
 
     @Test
@@ -38,6 +42,64 @@ class UpgradeSpringBootModulesRestTestClient_4_0_Tests implements RewriteTest {
                             TestRestTemplate testRestTemplate = null;
                         }
                         """
+                )
+        );
+    }
+
+    @Test
+    void dependencies() {
+        rewriteRun(
+                spec -> spec.beforeRecipe(withToolingApi()).expectedCyclesThatMakeChanges(2),
+                mavenProject("project",
+                        srcTestJava(
+                                //language=java
+                                java(
+                                        """
+                                        package com.yourorg;
+
+                                        import org.springframework.boot.test.web.client.TestRestTemplate;
+
+                                        class Demo {
+                                            TestRestTemplate testRestTemplate = null;
+                                        }
+                                        """,
+                                        """
+                                        package com.yourorg;
+
+                                        import org.springframework.boot.resttestclient.TestRestTemplate;
+
+                                        class Demo {
+                                            TestRestTemplate testRestTemplate = null;
+                                        }
+                                        """
+                                )
+                        ),
+                        //language=groovy
+                        buildGradle(
+                                """
+                                plugins {
+                                    id "java-library"
+                                }
+
+                                repositories {
+                                    mavenCentral()
+                                }
+                                """,
+                                """
+                                plugins {
+                                    id "java-library"
+                                }
+
+                                repositories {
+                                    mavenCentral()
+                                }
+
+                                dependencies {
+                                    testImplementation "org.springframework.boot:spring-boot-restclient"
+                                    testImplementation "org.springframework.boot:spring-boot-resttestclient"
+                                }
+                                """
+                        )
                 )
         );
     }
